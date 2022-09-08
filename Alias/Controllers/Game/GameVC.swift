c39a5eaff479816a62a5f666eb3d22b3bc2d1a0e
Anatoliy <tolxpams@gmail.com>
2022-09-08 17:23:01 +0300

    open game

---
+++ Alias/Controllers/Game/GameVC.swift
@@ -0,0 +1,29 @@
+//
+//  GameVC.swift
+//  Alias
+//
+//  Created by Anatoliy Khramchenko on 08.09.2022.
+//
+
+import UIKit
+
+class GameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Alias/Controllers/Start/StartVC.swift
+++ Alias/Controllers/Start/StartVC.swift
@@ -8,9 +8,38 @@
 import UIKit
 
 class StartVC: UIViewController {
-
+    
+    @IBOutlet weak var starImage: UIImageView!
+    @IBOutlet weak var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    @IBAction func startAction(_ sender: Any) {
+        let duration = TimeInterval(0.5)
+        let startScale = 0.5
+        let starScale = 15.0
+        UIView.animate(withDuration: duration/2, delay: 0.2, options: .curveEaseOut) { //make button smaller
+            self.startButton.transform = CGAffineTransform(scaleX: startScale, y: startScale)
+        } completion: { isFinish in
+            if isFinish {
+                UIView.animate(withDuration: duration/2, delay: 0, options: .curveEaseIn) { //make star bigger
+                    self.starImage.transform = CGAffineTransform(scaleX: starScale, y: starScale)
+                    self.startButton.alpha = 0
+                } completion: { isFinish in
+                    if isFinish {
+                        let gameVC = GameVC()
+                        gameVC.modalPresentationStyle = .fullScreen
+                        self.present(gameVC, animated: false) { //open game vc
+                            self.startButton.transform = .identity
+                            self.starImage.transform = .identity
+                            self.startButton.alpha = 1
+                        }
+                    }
+                }
+            }
+        }
     }
 
 }
