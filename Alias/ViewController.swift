e01fd89f063dd872af3d5d3968898de335afc002
Anatoliy <tolxpams@gmail.com>
2022-09-08 16:47:39 +0300

    Initial Commit

---
+++ Alias/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Alias
+//
+//  Created by Anatoliy Khramchenko on 08.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
