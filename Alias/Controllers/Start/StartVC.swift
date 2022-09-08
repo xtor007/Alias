5effdbd9b18a4176f5272c83122b850038ab916d
Anatoliy <tolxpams@gmail.com>
2022-09-08 17:01:49 +0300

    start vc

--- Alias/Controllers/Start/StartVC.swift
+++ Alias/Controllers/Start/StartVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  StartVC.swift
 //  Alias
 //
 //  Created by Anatoliy Khramchenko on 08.09.2022.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class StartVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
-
 }
-
